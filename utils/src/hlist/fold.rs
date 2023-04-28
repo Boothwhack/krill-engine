df0d5da303a61fc9ddad7c45c524376ea0ae58f5
Karoline <boothwhack@users.noreply.github.com>
2023-04-28 10:46:41 +0200

    Add try_fold test

--- utils/src/hlist/fold.rs
+++ utils/src/hlist/fold.rs
@@ -38,4 +38,26 @@ impl<Head, Tail> TryFold for (Head, Tail) {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::hlist;
+    use crate::hlist::TryFold;
+    use std::str::FromStr;
 
+    #[test]
+    fn try_fold() {
+        let folder = hlist!(
+            |a, i| {Some(a + i as f32)},
+            |a, s: &str| {Some(a + f32::from_str(s).ok()?)},
+            |a, f| {Some(a + f)},
+        );
+
+        let valid_list = hlist!(12u32, "4", 4.5f32);
+        let result = valid_list.try_fold(0.0f32, folder);
+        assert_eq!(Some(12.0 + 4.0 + 4.5), result);
+
+        let invalid_list = hlist!(12u32, "this is not a number", 4.5f32);
+        let result = invalid_list.try_fold(0.0f32, folder);
+        assert_eq!(None, result);
+    }
+}
